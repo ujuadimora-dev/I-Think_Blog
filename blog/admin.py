0b2ad6700e874f7c26921c8653fb5f0a02c4b4cf
Adimora Uju Louisa <adimorauju@gmail.com>
2023-06-02 23:36:57 +0000

    build Admin panel site 1

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
+# admin.site.register(Post).
