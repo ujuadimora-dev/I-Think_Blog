9ac7093c2b292ac4265682a38dcb37980f44e86e
Adimora Uju Louisa <adimorauju@gmail.com>
2023-06-03 00:26:26 +0000

    Update the admini site, add comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status','created_on')
     summernote_fields = ('content')
 
 
-# admin.site.register(Post).
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve-comments']
+
+def approve_comments(self, request, queryset):
+    queryset.update(approved=True)
+   
