9bdde9f1dcc26b0beec58b7dbc5de595cb5bb5a0
Adimora Uju Louisa <adimorauju@gmail.com>
2023-06-04 01:36:52 +0000

    create views for our page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post, Comment
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug, status=1)
+        comments = Comment.objects.filter(post=post, approved=True).order_by('created_on')
+        liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
